427e8ed7991492c0ac2d4dc35d837b25f801d180
Silver-Hawk <jesper.beltoft.lund@gmail.com>
2021-05-19 17:02:16 +0200

    fixed setup

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 import setuptools
 
-exec(open("./antisplode/_version.py").read()) # this will provide __version__
+exec(open("./antisplodge/_version.py").read()) # this will provide __version__
 
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
